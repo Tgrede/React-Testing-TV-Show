a759564e7b3815329ff0eaf74bb5fdcd8e84e813
Trenten grede <Trenten.grede@gmail.com>
2021-02-22 22:06:21 -0600

    tests that components renders without errors

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+import fetchShow from "./api/fetchShow";
 
 export default function App() {
   const [show, setShow] = useState(null);
@@ -14,19 +15,30 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+  // useEffect(() => {
+  //   // const fetchShow = () => {
+  //   //   axios
+  //   //     .get(
+  //   //       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //   //     )
+  //   //     .then(res => {
+  //   //       setShow(res.data);
+  //   //       setSeasons(formatSeasons(res.data._embedded.episodes));
+  //   //     });
+  //   // };
+  //   fetchShow();
+  // }, []);
+  const getData = () => {
+    fetchShow()
+    .then(res => {
+      setShow(res.data)
+      setSeasons(formatSeasons(res.data._embedded.episodes))
+    })
+  }
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    getData()
+  }, [])
+
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
